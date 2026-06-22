179a8ceed1b08d8044a423c9037b50e5316ea3db
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-06-22 18:54:25 +0100

    add DOM const selector and media selector function

---
+++ script.js
@@ -0,0 +1,12 @@
+const videoElement = document.getElementById('video');
+const buttonElement = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element, then play
+
+async function selectMediaStream(){
+  try{
+
+  } catch (error){
+    // Catch Error Here
+  }
+}
